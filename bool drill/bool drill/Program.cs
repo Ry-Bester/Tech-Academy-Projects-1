254e9e120d9627d607eb354a690214462801ef8c
Ryan Bester <Zelda0169@gmail.com>
2018-07-24 18:12:49 -0500

    bool drill

--- bool drill/bool drill/Program.cs
+++ bool drill/bool drill/Program.cs
@@ -12,12 +12,18 @@ namespace bool_drill
         {
             Console.WriteLine("What is your age?");
             int age = Convert.ToInt32(Console.ReadLine()); 
-            Console.WriteLine("Have you ever had a DUI? Please answer true or false");
-            string Dui = (Console.ReadLine());            
+            Console.WriteLine("Have you ever had a DUI?");
+            string Dui =(Console.ReadLine());
+            bool DuiTest = false;
+            if (Dui == "true" || Dui =="yes")
+            {
+                DuiTest = true;
+            }
             Console.WriteLine("How many speeding tickets do you have?");
             int tic = Convert.ToInt32(Console.ReadLine());
-            bool CD = (age > 15 && Dui == false && tic < 3);
-            Console.WriteLine("CD");
+            bool CD = (age > 15 && DuiTest == true && tic < 3);
+            Console.WriteLine( "Qualified?");
+            Console.WriteLine(CD);
             Console.ReadLine();
 
 
