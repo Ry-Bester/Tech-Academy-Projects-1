5fcbb34de0c23b0a11e07e4d5381ecfa35f3f605
Ryan Bester <Zelda0169@gmail.com>
2018-07-24 17:46:40 -0500

    drill help

---
+++ bool drill/bool drill/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bool_drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("Have you ever had a DUI? Please answer true or false");
+            string Dui = (Console.ReadLine());            
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tic = Convert.ToInt32(Console.ReadLine());
+            bool CD = (age > 15 && Dui == false && tic < 3);
+            Console.WriteLine("CD");
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
