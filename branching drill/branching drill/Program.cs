c3f4b517910245111fc5635462893661ba1fff37
Ryan Bester <Zelda0169@gmail.com>
2018-07-25 14:18:50 -0500

    branch drill

---
+++ branching drill/branching drill/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace branching_drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight.");
+            int wei = Convert.ToInt32(Console.ReadLine());
+
+            if (wei >= 50) 
+            {
+                Console.WriteLine("Package is to heavy to be shipped");
+                
+            }
+            
+            else if (wei < 50)
+            { 
+                Console.WriteLine("Weight Accepted");
+            }
+            
+            Console.WriteLine("Please enter the package height.");
+            int hei = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the package width.");
+            int wid = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the package length.");
+            int len = Convert.ToInt32(Console.ReadLine());
+            int Dim = hei * len * wid;
+            if (Dim >= 50)
+
+            {
+                Console.WriteLine("Package is to big to be shiped.");
+            }
+            
+            else if (Dim < 50) 
+
+            {  
+                Console.WriteLine("Dimensions Accepted!");
+
+            }
+
+            int total = Dim * wei / 100;
+            Console.WriteLine("Your estimated total for shipping this package is:  $" + total);
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
